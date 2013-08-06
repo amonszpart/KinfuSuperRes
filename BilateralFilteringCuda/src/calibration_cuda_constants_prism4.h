02b02a62c3ec21f954f30ac51c7cae0281f6e73c
aronmons <aron.monszpart@gmail.com>
2013-08-06 12:04:12 +0100

    good calib results

---
+++ BilateralFilteringCuda/src/calibration_cuda_constants_prism4.h
@@ -0,0 +1,42 @@
+// Calibration parameters from:
+// /media/Storage/workspace_ubuntu/rec/imgs_20130805_1047_calibPrism4/Calib_Results_ir_left.mat
+// /media/Storage/workspace_ubuntu/rec/imgs_20130805_1047_calibPrism4/Calib_Results_rgb_right.mat
+// /media/Storage/workspace_ubuntu/rec/imgs_20130805_1047_calibPrism4/Calib_Results_stereo_noreproj.mat
+
+#define FX_RGB 523.048350f
+#define FY_RGB 523.037845f
+#define CX_RGB 319.313152f
+#define CY_RGB 258.347998f
+
+#define K1_RGB 0.211853f
+#define K2_RGB -0.598351f
+#define K3_RGB 0.000754f
+#define K4_RGB -0.000743f
+#define K5_RGB 0.542609f
+#define ALPHA_RGB -0.002161f
+
+#define FX_D 588.486898f
+#define FY_D 588.729416f
+#define CX_D 314.509516f
+#define CY_D 242.997237f
+
+#define K1_D -0.094563f
+#define K2_D 0.387595f
+#define K3_D 0.000838f
+#define K4_D 0.001505f
+#define K5_D -0.494912f
+#define ALPHA_D -0.002096f
+
+#define R1 0.999998f
+#define R2 -0.002193f
+#define R3 -0.000235f
+#define R4 0.002193f
+#define R5 0.999998f
+#define R6 -0.000312f
+#define R7 0.000235f
+#define R8 0.000311f
+#define R9 1.000000f
+
+#define T1 -25.143760f
+#define T2 0.358017f
+#define T3 0.542178f
